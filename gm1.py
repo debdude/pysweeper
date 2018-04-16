fdd4508638dcdfd4b710baf974fe4a62f77f57a8
dmitryy <dy@deepmagic.io>
2018-04-16 13:29:46 +0300

    fix auto open bug

--- gm1.py
+++ gm1.py
@@ -112,35 +112,54 @@ class World():
             if self.mines[xx][yy]] )
 
     def open(self, x, y):  #try to open a cell
+        qs = None
+
         if self.mines[x][y]:
             self.map[x][y] = BLOWN
             return BOOM
 
-        if self.map[x][y] < UNKNOWN: #already marked
-            return OK   
+        flagged_ns, unknown_ns = [], []
+        #already has number, maybe can open neighbors
+        if self.map[x][y] < UNKNOWN and self.map[x][y] > 0: 
+            for xx, yy in self.get_neighbors(x, y):
+                if self.map[xx][yy] == FLAG:
+                    flagged_ns.append((xx, yy))
+                elif self.map[xx][yy] == UNKNOWN:
+                    unknown_ns.append((xx, yy))            
+            #all surrounding mines marked (supposedly)
+            #add remaining neighbors to q
+            if len(flagged_ns) >= self.map[x][y]: 
+                qs = set([xx*1000+yy for xx,yy in unknown_ns])
+                debug(str(qs))
+            else:
+                #nope, cannot safely open around
+                return OK   
 
-        qs = set()  #set is actually a unique queue
-        qs.add(x*1000 + y)
-
+        #init if not inited earlier
+        #set is actually a unique queue
+        qs = qs or set((x*1000 + y, ))  
+        
         #term throws up sometimes in concurrent output, disable timer
         signal.alarm(0) 
 
         while len(qs):            
             xy = qs.pop()
             x, y = xy//1000, xy % 1000
+            #can still trigger BOOM if opening from number
+            #and flags were misplaced
+            if self.mines[x][y]: 
+                self.map[x][y] = BLOWN
+                return BOOM             
             nmines = self.get_count(x, y) 
             self.map[x][y] = nmines
             if nmines == 0:
-                for xx, yy in self.get_neighbors_u(x, y):                
+                for xx, yy in self.get_neighbors_u(x, y):   
+                    
+                    
                     qs.add(xx*1000+yy)
 
         signal.alarm(1)  #reenable timer
         return WIN if self.iswin() else OK
-
-
-
-
-
 
     def get_neighbors(self, x, y):
         minx = max(0, x-1)
@@ -161,14 +180,10 @@ class World():
     #         in self.get_neighbors(x,y) 
     #         if (self.map[xx][yy] == UNKNOWN and [xx,yy] not in self.q)]
 
-
     def iswin(self):
         closed = [0 for y in range(self.sy) for x in range(self.sx) 
             if self.map[x][y] >= UNKNOWN]
         return True if len(closed)==self.nmines else False
-
-
-
 
 
 
@@ -182,19 +197,7 @@ def getch():   # define non-Windows version
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
-
-
-def cpos():
-    global ox, oy
-    try:
-        term.pos(cy + 1 + OFFY, cx +1)
-        term.write(* tiles[ world.map[cx][cy] ])
-        term.pos(cy + 1 + OFFY, cx +1)
-    except RuntimeError:
-        sleep(0.2)
-        cpos()  #timing glitch in term, just retry
-
-
+#actions
 def up():
     global cx, cy
     cy = max(cy-1, 0)
@@ -230,20 +233,29 @@ def toggle():
     cpos()
     return OK 
 
+def brk():
+    return BREAK
 
+# /actions
+
+
+#display
 def status(text = '', *args):
     term.pos(2 + world.sy + OFFY, 8)
     term.write(term.red + '¤:' + term.white )
     term.write('%d/%d  '%(world.nmines - world.mines_marked(), 
                 world.nmines))
-    term.write(text, *args)  
-            
+    term.write(text, *args)              
     cpos()
-
 
 def help(text = term.dim + "move: wasd/toggle: SPACE/open: ENTER"):
     term.pos(3 + world.sy + OFFY, 0)
     term.write(text)    
+
+def debug(text):
+    term.pos(3 + world.sy + OFFY, 0)
+    term.write(text)    
+
 
 def timeout_handler(sg, frame):
     term.pos(2 + world.sy + OFFY, 1)
@@ -251,6 +263,24 @@ def timeout_handler(sg, frame):
     term.write(term.white + '%i '%(time.time() - starttime))
     signal.alarm(1)
     cpos()
+
+def cpos():
+    #position cursor properly and redraw that tile
+    try:
+        term.pos(cy + 1 + OFFY, cx +1)
+        term.write(* tiles[ world.map[cx][cy] ])
+        term.pos(cy + 1 + OFFY, cx +1)
+    except RuntimeError:
+        sleep(0.2)
+        cpos()  #timing glitch in term, just retry lol
+
+def head():
+    term.pos(1,1)
+    term.write(term.center(term.green + 
+        'SYSWEEPER %ix%i'%(sx, sy)))
+    term.write(term.off)
+
+# /display
 
 
 def gameloop(x = 80, y=30, mm = 40):
@@ -267,7 +297,8 @@ def gameloop(x = 80, y=30, mm = 40):
         'a': left,
         'd': right,
         '\r': enter,
-        ' ': toggle
+        ' ': toggle,
+        '\03': brk
     }
 
     world = World(x, y, mm)
@@ -284,7 +315,7 @@ def gameloop(x = 80, y=30, mm = 40):
     signal.signal(signal.SIGALRM, timeout_handler)
     signal.alarm(1)
 
-    while key != '\03' and result == OK:
+    while result == OK:
         if redraw: 
             world.draw()
             redraw = False
@@ -294,40 +325,26 @@ def gameloop(x = 80, y=30, mm = 40):
         if key in actions:
             result = actions[key]()
 
-
-    signal.alarm(0)
+    signal.alarm(0) #stop timer!
     world.reveal()
-    if key == '\03':
-        return BREAK
     return result
-
-
-def head():
-    term.pos(1,1)
-    term.write(term.center(term.green + 
-        'SYSWEEPER %ix%i'%(sx, sy)))
-    term.write(term.off)
-
 
 
 def main():
     import argparse
 
     global sx, sy, nm
-    more = True
     sx, sy, nm = 50, 20, 10
 
     parser = argparse.ArgumentParser(description='Console minesweeper',
         argument_default = 'auto')
     parser.add_argument('size', 
-            # metavar='<size: auto | s | m | l | xl>', 
             choices = ['s', 'm', 'l', 'xl', 'auto'],
             default = 'auto',
             nargs = '?', #make optional
             help='field size, default: auto fill terminal')
 
     args = parser.parse_args()
-
 
     worldsizes = {
         's': (15, 10, 15),
@@ -340,6 +357,7 @@ def main():
     sx, sy, nm = worldsizes[args.size]
     term.clear()
     head()
+    more = True
     while more:    
         result = gameloop(sx, sy, nm)
         if result == BREAK:
@@ -357,9 +375,8 @@ def main():
             key = getch()
         more = (key in 'yY')
 
-    print('')
-    term.write(term.off)
-    print('k bye')
+    term.writeLine(" ")
+    term.writeLine(term.off + "k bye")
 
 
 
@@ -388,7 +405,7 @@ def maptest():
     w.open(1,6)
     w.draw()
     res = w.open(5, 3)    
-    print(res == BOOM)
+    assert res == BOOM
     w.reveal()
 
 if __name__ == "__main__":
